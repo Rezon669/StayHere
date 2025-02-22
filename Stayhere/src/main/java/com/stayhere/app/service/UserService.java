35988f857f7126aac12aa3c746d7964b11d3ee77
Rezon Ventru <rezon449@gmail.com>
2025-02-22 13:23:32 +0530

    Added HATEOAS functionality

--- Stayhere/src/main/java/com/stayhere/app/service/UserService.java
+++ Stayhere/src/main/java/com/stayhere/app/service/UserService.java
@@ -1,9 +1,14 @@
 package com.stayhere.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,112 +21,117 @@ import com.stayhere.app.model.UserDto;
 import com.stayhere.app.repo.UserRepository;
 import com.stayhere.app.security.JwtUtil;
 
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.CacheEvict;
-
 @Service
 public class UserService {
 
-	private static final Logger log = LogManager.getLogger(UserService.class);
-	private final UserRepository userRepo;
-	private final AuthenticationManager authenticationManager;
-	private final JwtUtil jwtUtil;
-	private final PasswordEncoder passwordEncoder;
+    private static final Logger log = LogManager.getLogger(UserService.class);
+    private final UserRepository userRepo;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
-	public UserService(UserRepository userRepo, AuthenticationManager authenticationManager, JwtUtil jwtUtil,
-			PasswordEncoder passwordEncoder) {
-		this.userRepo = userRepo;
-		this.authenticationManager = authenticationManager;
-		this.jwtUtil = jwtUtil;
-		this.passwordEncoder = passwordEncoder;
+    public UserService(UserRepository userRepo, AuthenticationManager authenticationManager, JwtUtil jwtUtil,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @CacheEvict(value = "usersCache", key = "'users'")
+    @CachePut(value = "usersCache", key = "#result.userid")
+    public UserDto addUser(UserDto user) {
+        userRepo.findByUsernameOrEmail(user.getUsername(), user.getEmail()).ifPresent(existingUser -> {
+            throw new CustomException("User already exists", "A user with the given username or email already exists.");
+        });
+
+        // Encode the password
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+     
+
+        try {
+            UserDto savedUser = userRepo.save(user);
+            savedUser.setPassword("SECRET"); // Mask password in response
+            log.info("User added successfully: {}", savedUser);
+            return savedUser; // Cache the newly added user
+        } catch (Exception e) {
+            log.error("Error while saving user: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Cacheable(value = "usersCache", key = "'users'")
+    public List<UserDto> getUsers() {
+        List<UserDto> users = userRepo.findAll();
+        log.info("Fetched all user details successfully.");
+        users.forEach(user -> user.setPassword("SECRET")); // Mask passwords
+        return users;
+    }
+
+    @Caching(evict = {
+    	    @CacheEvict(value = "usersCache", key = "'users'"), // Evict the list cache
+    	    @CacheEvict(value = "usersCache", key = "#userId")  // Evict the individual user cache
+    	})
+    public UserDto deleteUser(long userId) {
+        return userRepo.findById(userId).map(user -> {
+            userRepo.deleteById(userId);
+            log.info("User deleted successfully with ID: {}", userId);
+            user.setPassword("SECRET");
+            return user;
+        }).orElseThrow(() -> new CustomException("User not found", "No user exists with the given ID: " + userId));
+    }
+
+    @CacheEvict(value = "usersCache", key = "'users'")
+    @CachePut(value = "usersCache", key = "#userId")
+//    @Caching(evict = {
+//            @CacheEvict(value = "usersCache", key = "'users'") // Evict cached user list
+//        },
+//        put = {
+//            @CachePut(value = "usersCache", key = "#updateUser.userId") // Update cache for specific user
+//        }
+//    )
+    public UserDto updateUser(long userId, @Validated UserDto updateUser) {
+        return userRepo.findById(userId).map(existingUser -> {
+            // Update user details
+            existingUser.setUsername(updateUser.getUsername());
+            existingUser.setFirstname(updateUser.getFirstname());
+            existingUser.setLastname(updateUser.getLastname());
+            existingUser.setEmail(updateUser.getEmail());
+
+            // Update password only if it has been changed
+            if (!updateUser.getPassword().isEmpty() && !passwordEncoder.matches(updateUser.getPassword(), existingUser.getPassword())) {
+                existingUser.setPassword(passwordEncoder.encode(updateUser.getPassword()));
+            }
+
+            existingUser.setPhonenumber(updateUser.getPhonenumber());
+            existingUser.setRole(updateUser.getRole());
+
+            UserDto updatedUser = userRepo.save(existingUser);
+            log.info("User details updated successfully for ID: {}", userId);
+            updatedUser.setPassword("SECRET"); // Mask password in response
+            return updatedUser;
+        }).orElseThrow(() -> new CustomException("User not found", "No user exists with the given ID: " + userId));
+    }
+
+    public String authenticateUser(String email, String password) {
+        UserDto user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), password));
+
+        if (authentication.isAuthenticated()) {
+            return jwtUtil.generateToken(user.getUsername(), user.getRole(), user.getEmail());
+        }
+
+        throw new RuntimeException("Invalid username or password");
+    }
+
+	public Optional<UserDto> getUserById(Long userId) {
+		Optional<UserDto> user = userRepo.findById(userId);
+		return user;
 	}
 
-	@CachePut(value = "usersCache", key = "#user.id")
-	public UserDto addUser(UserDto user) {
-		// Check if user already exists by username or email
-		userRepo.findByUsernameOrEmail(user.getUsername(), user.getEmail()).ifPresent(existingUser -> {
-			throw new CustomException("User already exists", "A user with the given username or email already exists.");
-		});
-
-		// Encode the password and save the user
-		String userPassword = user.getPassword();
-		String encodedPassword = passwordEncoder.encode(user.getPassword());
-		user.setPassword(encodedPassword);
-		log.info("Encoded password: {}", encodedPassword);
-
-		try {
-			userRepo.save(user);
-			user.setPassword(userPassword);
-			log.info("User added successfully: {}", user);
-			return user;
-		} catch (Exception e) {
-			log.error("Error while saving user: {}", e.getMessage(), e);
-			throw e;
-		}
-	}
-
-	@Cacheable(value = "usersCache", key = "'users'")
-	public List<UserDto> getUsers() {
-		List<UserDto> users = userRepo.findAll();
-		log.info("Fetched all user details successfully.");
-		users.forEach(user -> user.setPassword(""));
-		return users;
-	}
-
-	@CacheEvict(value = "usersCache", key = "#userId")
-	public UserDto deleteUser(long id) {
-		// Delete the user if it exists, else throw exception
-		return userRepo.findById(id).map(user -> {
-			userRepo.deleteById(id);
-			log.info("User deleted successfully with ID: {}", id);
-			return user;
-		}).orElseThrow(() -> new CustomException("User not found", "No user exists with the given ID: " + id));
-	}
-
-	@CachePut(value = "usersCache", key = "#userId")
-	public UserDto updateUser(long id, @Validated UserDto updateUser) {
-		// Find the existing user and update their details
-		return userRepo.findById(id).map(existingUser -> {
-			existingUser.setUsername(updateUser.getUsername());
-			existingUser.setFirstname(updateUser.getFirstname());
-			existingUser.setLastname(updateUser.getLastname());
-			existingUser.setEmail(updateUser.getEmail());
-
-			String userPassword = updateUser.getPassword();
-			// Update password only if it's different
-			if (!updateUser.getPassword().equals(existingUser.getPassword())) {
-				existingUser.setPassword(passwordEncoder.encode(updateUser.getPassword()));
-			}
-
-			existingUser.setPhonenumber(updateUser.getPhonenumber());
-			existingUser.setRole(updateUser.getRole());
-
-			// Save updated user
-			UserDto updatedUser = userRepo.save(existingUser);
-			log.info("User details updated successfully for ID: {}", id);
-			updatedUser.setPassword(userPassword);
-			return updatedUser;
-		}).orElseThrow(() -> {
-			log.error("User not found with ID: {}", id);
-			return new CustomException("User not found", "No user exists with the given ID: " + id);
-		});
-	}
-
-	public String authenticateUser(String email, String password) {
-		// Find user by email
-		UserDto user = userRepo.findByEmail(email)
-				.orElseThrow(() -> new RuntimeException("User not found with email: " + email));
-
-		// Authenticate the user using the provided password
-		Authentication authentication = authenticationManager
-				.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), password));
-
-		// If authentication is successful, generate JWT token
-		if (authentication.isAuthenticated()) {
-			return jwtUtil.generateToken(user.getUsername(), user.getRole(), user.getEmail());
-		}
-
-		throw new RuntimeException("Invalid username or password");
-	}
+	
 }
